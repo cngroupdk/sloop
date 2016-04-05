3340c1d2ec0a6d5b9bfa3635d3932547e4e0c11d
mikusd <mikusd@cngroup.dk>
2016-04-05 22:50:33 +0200

    configuration for jobfair

--- src/components/configuration.js
+++ src/components/configuration.js
@@ -1,11 +1,11 @@
 export const facebookAppID = '1589187544692382';
-export const facebookLimit = 5;
-export const twitterLimit = 10;
+export const facebookLimit = 50;
+export const twitterLimit = 50;
 export const refreshIntervalTime = 360 * 1000;
 export const slideIntervalTime = 15 * 1000;
 export const sources = {
   facebook: [
-    'cngroupzl', 'cngroupcz', 'cnuniversity'
+    'cngroupzl', 'cngroupcz'
   ],
   twitter: [
   ]
