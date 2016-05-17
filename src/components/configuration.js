2728c4e30c0d062c34f15db5a02918f5ae3d81d9
mikusd <mikusd@cngroup.dk>
2016-05-17 13:35:58 +0200

    transparent bug +

--- src/components/configuration.js
+++ src/components/configuration.js
@@ -1,12 +1,13 @@
 export const facebookAppID = '1589187544692382';
-export const facebookLimit = 50;
-export const twitterLimit = 50;
+export const facebookLimit = 15;
+export const twitterLimit = 15;
 export const refreshIntervalTime = 360 * 1000;
 export const slideIntervalTime = 15 * 1000;
 export const sources = {
   facebook: [
-    'cngroupzl', 'cngroupcz'
+    'cngroupzl', 'cngroupcz', 'cnuniversity'
   ],
   twitter: [
+    'cngroup_cz'
   ]
 };
